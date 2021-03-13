c2837f9938a50c072c60691db0c8cbdcc2a0c7d4
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-03-14 00:37:13 +0530

    version 1(basics of React)

---
+++ src/Components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react" ;
+
+import Heading from "./Header" ;
+import Footer from "./Footer" ;
+import Note from "./Note" ;
+
+function App(){
+
+
+return (
+
+    <div> 
+<Heading /> 
+
+<Note > </Note>
+
+ <Footer / >
+ </div>
+
+) ;
+
+}
+
+export default App ;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react" ;
+
+var today= new Date() ;
+var year= today.getFullYear() ;
+
+
+ 
+
+function Footer() { 
+
+    return (<footer>
+
+<p>Copyright© {year}</p>
+
+</footer>) 
+
+    ;
+ }
+
+
+ export default Footer ;
